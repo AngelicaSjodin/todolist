98d83da0c20c14c43d5d9b79e76b0ab1b253e382
AngelicaSjodin <angelic.sjodin@gmail.com>
2024-09-30 16:09:15 +0200

    redo

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 const addButton=document.getElementById('addButton');
+const input = document.getElementById('input');
+const list = document.getElementById("list");
 
 
-document.getElementById("addButton").addEventListener("click",addToList)
-document.getElementById("input").addEventListener("keypress",function(event){
-    if (event.key === "enter") {
-        addToList();
-    }
-});
-
-function addToList(){
-    const input = document.getElementById('input');
-    const inputText=input.value();
-
-    if (inputText !== ""){
-        const list = document.getElementById("list");
-        const listItem = document.createElement("li");
-
-    }
-}
 
 
