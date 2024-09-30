588a872d4f01464a6c53d7c85dc34cbfffa8c6d5
AngelicaSjodin <angelic.sjodin@gmail.com>
2024-09-30 14:05:04 +0200

    starting function

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const addButtonField=document.getElementById('addButton');
-const addInputField=document.getElementById('addInput');
+const addButton=document.getElementById('addButton');
+
+
+document.getElementById("addButton").addEventListener("click",addToList)
+document.getElementById("input").addEventListener("keypress",function(event){
+    if (event.key === "enter") {
+        addToList();
+    }
+});
+
+function addToList(){
+    const input = document.getElementById('input');
+    const inputText=input.value();
+
+    if (inputText !== ""){
+        const list = document.getElementById("list");
+        const listItem = document.createElement("li");
+
+    }
+}
+
+
